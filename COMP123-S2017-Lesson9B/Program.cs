8e9b30564ffb87108e1b63f269cb09cdc86dcc63
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-18 16:49:33 -0400

    Implemented the SuperVillain class in the Driver class

--- COMP123-S2017-Lesson9B/Program.cs
+++ COMP123-S2017-Lesson9B/Program.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 /*
  * Name: Tom Tsiliopoulos
  * Date: July 11, 2017
- * Description: Demo for Lesson 9
- * Version: 0.7 - Modified Driver class to implement SuperHero
+ * Description: Demo for Lesson 10
+ * Version: 0.8 - Implemented the SuperVillain class
  */
 
 namespace COMP123_S2017_Lesson9B
@@ -17,12 +17,18 @@ namespace COMP123_S2017_Lesson9B
     {
         static void Main(string[] args)
         {
-            SuperHero superHero = new SuperHero("Super Dude", 20);
+            SuperHero superHero = new SuperHero("SuperHero Dude", 20);
             superHero.AddPower("Spider Climbing", 50);
             superHero.AddPower("Webcasting", 40);
             Console.WriteLine(superHero.ToString());
+            Console.WriteLine();
 
 
+            SuperVillain superVillain = new SuperVillain("SuperVillain Dude", 20);
+            superVillain.AddPower("Shadow Casting", 50);
+            superVillain.AddPower("Mind Control", 40);
+            superVillain.AddPower("Combat Senses", 30);
+            Console.WriteLine(superVillain.ToString());
         }
     }
 }
